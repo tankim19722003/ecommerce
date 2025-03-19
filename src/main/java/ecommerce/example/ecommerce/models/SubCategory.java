1055503507c506a1fdc1c0d09ca36ead967942eb
Kim Ngọc Tân <tankim071902@gmail.com>
2025-03-19 16:23:45 +0700

    fixed subcategories bug

--- src/main/java/ecommerce/example/ecommerce/models/SubCategory.java
+++ src/main/java/ecommerce/example/ecommerce/models/SubCategory.java
@@ -19,6 +19,7 @@ import java.util.List;
 public class SubCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "name", nullable = false, unique = true)
