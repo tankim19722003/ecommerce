7dc7339326e3aa70cfc52ce0956f6e560a85f5b4
Kim Ngọc Tân <tankim071902@gmail.com>
2025-04-05 21:16:33 +0700

    fix load data from db

--- src/main/java/ecommerce/example/ecommerce/Repo/VoucherRepo.java
+++ src/main/java/ecommerce/example/ecommerce/Repo/VoucherRepo.java
@@ -1,14 +1,17 @@
-    package ecommerce.example.ecommerce.Repo;
-
-    import ecommerce.example.ecommerce.models.Voucher;
-    import org.springframework.data.jpa.repository.JpaRepository;
-    import org.springframework.data.jpa.repository.Query;
-    import org.springframework.data.repository.query.Param;
-
-    import java.util.List;
-
-    public interface VoucherRepo extends JpaRepository<Voucher, Long> {
-
-//        @Query("SELECT v FROM Voucher v WHERE v.product.id = :productId AND v.endDate > CURRENT_TIMESTAMP AND v.startDate <= CURRENT_TIMESTAMP")
-//        List<Voucher> findVoucherByProductId(@Param("shopId") Long shopId);
-    }
+//package ecommerce.example.ecommerce.Repo;
+//
+//import ecommerce.example.ecommerce.models.Voucher;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+//
+//import java.util.List;
+//
+//public interface VoucherRepo extends JpaRepository<Voucher, Long> {
+//
+//    @Query("SELECT v FROM Voucher v \n" +
+//            "    WHERE v.product.id = :productId \n" +
+//            "      AND v.endDate > CURRENT_TIMESTAMP \n" +
+//            "      AND v.startDate <= CURRENT_TIMESTAMP")
+//    List<Voucher> findVoucherByProductId(@Param("shopId") Long shopId);
+//}
